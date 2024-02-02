e991d2091817ec011f3af2b9f6836d4605e6c2e5
Soulbolt <soulbolt@gmail.com>
2024-02-02 07:39:24 -0700

    Refactor tro structure files in a component folder and move components into their own files. Add default to each export file and import them into the App.js file accordingly.

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  // Add item to items array[]
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+  // Delete time from items array[]
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  // toggle item check to line-cross
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  // Clear list on click
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItems={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
