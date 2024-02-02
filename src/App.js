74303be1194d67dc5e83ca9b529a962d05b461f0
Soulbolt <soulbolt@gmail.com>
2024-02-02 07:23:41 -0700

    Add confirmation window to verify if use is sure about clearing the list. Clea list if hit okay, cancel if cancelled.

--- src/App.js
+++ src/App.js
@@ -21,7 +21,11 @@ export default function App() {
   }
   // Clear list on click
   function handleClearList() {
-    setItems([]);
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
   }
 
   return (
